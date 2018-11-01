c349ed362a2df4abaff1bca0ed299218ec27313b
Adrian J. Fox <themage107@gmail.com>
2018-11-01 16:57:18 -0700

    Take a high quality Screenshot of your level with the Unity Camera
    Unity 5 only. Take a screenshot of the level, will be large with 4 (300
    DPI).

---
+++ screenShot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenShot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("screenShotCapture");
+    }
+	
+    IEnumerator screenShotCapture()
+    {
+        while (true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Application.CaptureScreenshot(@"C:\Users\COMPUTER NAME HERE\Desktop\o.png", 4);
+            }
+
+            yield return new WaitForSecondsRealtime(0.016f);
+        }
+    }
+	
+}
